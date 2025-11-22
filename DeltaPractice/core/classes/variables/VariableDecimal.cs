46249eb5d3065a7c78d385139451f9615eff60c9
cenfraGit <161994865+cenfraGit@users.noreply.github.com>
2025-11-22 14:16:00 -0600

    refactor: using AVariableNumeric abstract class in numeric variable classes.

---
+++ DeltaPractice/core/classes/variables/VariableDecimal.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace core.classes.variables;
+
+/// <summary>
+/// Represents a random decimal between the range of LowerLimit and UpperLimit.
+/// </summary>
+public class VariableDecimal : AVariableNumeric<float>
+{
+    public VariableDecimal(float limitLower, float limitUpper) : base(limitLower, limitUpper)
+    { }
+    
+    public override void Recalculate()
+    {
+        Random random = new Random();
+        float randomVal = (float)random.NextDouble();
+        float range = LimitUpper - LimitLower;
+        float randomDouble = LimitLower + (range * randomVal);
+        randomDouble = (float)Math.Round((decimal)randomDouble, 3);
+        this.Value = randomDouble;
+    }
+}
--- DeltaPractice/core/classes/variables/VariableInteger.cs
+++ DeltaPractice/core/classes/variables/VariableInteger.cs
@@ -3,78 +3,16 @@ using System;
 namespace core.classes.variables;
 
 /// <summary>
-/// Represents a random integer between the range of LowerLimit and UpperLimit
+/// Represents a random integer between the range of LowerLimit and UpperLimit (inclusive).
 /// </summary>
-public class VariableInteger : IVariable<int>
+public class VariableInteger : AVariableNumeric<int>
 {
-    // --------------------------------------------------------------------------------
-    // fields
-    // --------------------------------------------------------------------------------
-
-    private int _limitLower;
-    private int _limitUpper;
-
-    // --------------------------------------------------------------------------------
-    // properties
-    // --------------------------------------------------------------------------------
-
-    public int Value { get; set; }
-
-    public int LimitLower
-    {
-        get => this._limitLower;
-        set
-        {
-            ArgumentNullException.ThrowIfNull(value);
-
-            if (value > this._limitUpper)
-                throw new ArgumentException($"Lower limit can't be greater than upper limit. ({value} > {_limitUpper}).");
-
-            this._limitLower = value;
-        }
-    }
-
-    public int LimitUpper
-    {
-        get => this._limitUpper;
-        set
-        {
-            ArgumentNullException.ThrowIfNull(value);
-
-            if (value < this._limitLower)
-                throw new ArgumentException($"Upper limit can't be less than lower limit. ({value} < {_limitLower}).");
-
-            this._limitUpper = value;
-        }
-    }
-
-    // --------------------------------------------------------------------------------
-    // constructor
-    // --------------------------------------------------------------------------------
-
-    public VariableInteger(int limitLower, int limitUpper)
-    {
-        ArgumentNullException.ThrowIfNull(limitLower);
-        ArgumentNullException.ThrowIfNull(limitUpper);
-
-        this._limitLower = limitLower;
-        this._limitUpper = limitUpper;
-
-        // confirm if values are valid
-        this.LimitLower = limitLower;
-        this.LimitUpper = limitUpper;
-    }
-
-    // --------------------------------------------------------------------------------
-    // methods
-    // --------------------------------------------------------------------------------
-
-    /// <summary>
-    /// Recalculates the Value of the variable.
-    /// </summary>
-    public void Recalculate()
+    public VariableInteger(float limitLower, float limitUpper) : base(limitLower, limitUpper)
+    { }
+    
+    public override void Recalculate()
     {
         Random random = new Random();
-        this.Value = random.Next(this.LimitLower, this.LimitUpper + 1);
+        this.Value = random.Next((int)this.LimitLower, (int)this.LimitUpper + 1);
     }
 }
